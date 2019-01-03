033faaa94cd4023552b251b533c6df77232eb4b4
Bertrand Steinmetz <bstenm@hotmail.com>
2019-01-02 20:33:51 -0500

    adding build folder in repo for travis to deploy it

---
+++ build/precache-manifest.c7fcac0463841e746287772a2173ecc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5271d786063d57da2d60",
+    "url": "/static/js/main.5271d786.chunk.js"
+  },
+  {
+    "revision": "0968d3c3bd7014ed6f84",
+    "url": "/static/js/1.0968d3c3.chunk.js"
+  },
+  {
+    "revision": "5271d786063d57da2d60",
+    "url": "/static/css/main.21c4a130.chunk.css"
+  },
+  {
+    "revision": "d589d117e697e7a2fe8cc1f3ce72dc29",
+    "url": "/index.html"
+  }
+];
